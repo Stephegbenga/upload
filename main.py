e52c0c78eec5f7b08aeede41ce58cb17300111b0
Stephegbenga <78284135+Stephegbenga@users.noreply.github.com>
2022-03-18 17:37:11 +0100

    update

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, request
 from decouple import config
 from pprint import pprint
-from server import createorder
 import requests
 import hashlib
 import base64
@@ -18,6 +17,27 @@ print(password)
 app = Flask(__name__)
 
 variant_token = "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"
+
+def createorder(customerid, products):
+    token = accesstoken().json()['access_token']
+    url = "https://api.malfini.com/api/v4/order"
+    payload = json.dumps({
+        "invoiceDeliveryTypeId": 3,
+        "addressId": 157441,
+        "deliveryId": 75,
+        "paymentId": 9,
+        "customOrderId": customerid,
+        "products": products
+    })
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload).json()
+    return response
+
+
 
 def accesstoken():
     url = "https://api.malfini.com/api/v4/api-auth/login"
