e11bed1d30e5c785817f8699350ef6e79c242a3c
Stephegbenga <78284135+Stephegbenga@users.noreply.github.com>
2022-03-19 18:52:23 +0100

    update

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def accesstoken():
 
 # This creates an address of that user on Malfini
 def createaddress(name, street, countrycode, zipcode, city, phoneno, email):
-    token = accesstoken()
+    # token = accesstoken()
     url = "https://api.malfini.com/api/v4/address"
     phonenumberprefix = phonenumbers.parse("100993393939", countrycode).country_code
     phonenumber = f'+{phonenumberprefix}-{phoneno}'
